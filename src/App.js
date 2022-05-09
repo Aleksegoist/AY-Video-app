1309db95c2a632602f03f7e067c377504914d183
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-09 21:07:46 +0300

    add

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Account from './pages/Account'
+import Account from './pages/Account';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
     return (
@@ -15,7 +16,14 @@ function App() {
                     <Route path='/' element={<Home />} />
                     <Route path='/login' element={<Login />} />
                     <Route path='/signup' element={<Signup />} />
-                    <Route path='/account' element={<Account />} />
+                    <Route
+                        path='/account'
+                        element={
+                            <ProtectedRoute>
+                                <Account />
+                            </ProtectedRoute>
+                        }
+                    />
                 </Routes>
             </AuthContextProvider>
         </>
