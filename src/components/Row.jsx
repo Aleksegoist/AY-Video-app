c8f7edd0d2a404b9390b9e33978daec91f8202b8
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-09 15:15:02 +0300

    add

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Movie from './Movie';
+import { MdS } from 'react-icons';
 
 const Row = ({ title, fetchURL }) => {
     const [movies, setMovies] = useState([]);
+
     useEffect(() => {
         axios.get(fetchURL).then((response) => {
             setMovies(response.data.results);
@@ -13,20 +16,12 @@ const Row = ({ title, fetchURL }) => {
         <>
             <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
             <div className='relative flex items-center'>
-                <div id={'slider'}>
+                <div
+                    id={'slider'}
+                    className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'
+                >
                     {movies.map((item, id) => (
-                        <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-                            <img
-                                className='w-full h-auto block'
-                                src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
-                                alt={item?.title}
-                            />
-                            <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white '>
-                                <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
-                                    {item?.title}
-                                </p>
-                            </div>
-                        </div>
+                        <Movie key={id} item={item} />
                     ))}
                 </div>
             </div>
